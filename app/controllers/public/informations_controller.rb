2546c19d2c0039a4b6108903278dc0523bdc8db4
EC2 Default User <ec2-user@ip-172-31-19-208.ap-northeast-1.compute.internal>
2023-11-11 12:45:43 +0000

    [add]information show

--- app/controllers/public/informations_controller.rb
+++ app/controllers/public/informations_controller.rb
@@ -5,9 +5,4 @@ class Public::InformationsController < ApplicationController
   def show
   end
 
-  private
-
-  def information_params
-    params.require(:information).permit(:campany_name, :name, :phone_number, :post_code, :address, :email, :establishment_date, :capital, :business_content)
-  end
 end
